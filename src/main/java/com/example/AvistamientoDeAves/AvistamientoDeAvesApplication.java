9c2ff0f9284d0ec813e1a905e16a057eb6f7f738
Neger008 <alfredo.full.cf@gmail.com>
2019-05-22 21:55:56 -0500

    iniciando Spring boot

---
+++ src/main/java/com/example/AvistamientoDeAves/AvistamientoDeAvesApplication.java
@@ -0,0 +1,13 @@
+package com.example.AvistamientoDeAves;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AvistamientoDeAvesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AvistamientoDeAvesApplication.class, args);
+	}
+
+}
